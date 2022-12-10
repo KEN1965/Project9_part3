6c6bc1fa4c924aa43bf0d16b082a0bdc4ee657a2
Kenichi Takahama <hammer528@msn.com>
2022-12-10 13:01:09 +0900

    Project9_part3「Animating complex shapes with AnimatablePair」finish

--- Project9_part3/ContentView.swift
+++ Project9_part3/ContentView.swift
@@ -7,40 +7,55 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: Double
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
     
-    var animatableData: Double {
-        get { insetAmount }
-        set { insetAmount = newValue }//滑らかになったかな？
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows),Double(columns))
+        }
+        set{
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
     }
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
         
+        for row in 0..<columns {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
         return path
     }
 }
 
 struct ContentView: View {
     //Project9_part3 やっていきやしょう(๑>◡<๑)
-    //カスタム形状(台形)を作っていきます
+    //行と列を使用して新しい形状を作っていきます
     //表示させていきます
-    @State private var insetAmount = 50.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 200, height: 100)
+        Checkerboard(rows: rows, columns: columns)
             .onTapGesture {
-                withAnimation{//アニメーションが効いていない？？
-                    insetAmount = Double.random(in: 10...90)
-                }
+                withAnimation(.linear(duration: 3)) {//前回のようにランダムにしてみます
+                    rows = Int(Double.random(in: 4...16))
+                    columns = Int(Double.random(in: 4...16))
+                }//一瞬で変わるのでアニメーションさせるには・・・
             }
     }
 }
